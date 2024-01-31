b69b430a8f65037f2f810eb4d7ba2b5b2f4a1796
Hugo Culot <hugoculot.dev@gmail.com>
2024-01-31 19:05:15 +0100

    Ajout de la Sidebar.

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,90 @@
 <script setup>
-// Pour L'API
-import { ref, computed, onMounted } from 'vue'
 
-// Query pour ce que l'on tape pour trouver l'animé 
-const query = ref ('')
-// Les animé qu'on ajout a notre list
-const my_anime = ref ([])
-// Tout les resulta après le result de notre query
-const search_results = ref ([])
+// Sidebar
+import Sidebar from "./components/sidebar/Sidebar.vue"
+import { sidebarWidth } from './components/sidebar/state'
 
 
-// Permet de lire de A-Z
-const my_anime_asc = computed(() => {
-  return my_anime.value.sort((a, b) => {
-    return a.title.localeCompare(b.title)
+
+
+
+
+
+
+
+  // Pour L'API
+  import { ref, computed, onMounted } from 'vue'
+
+  // Query pour ce que l'on tape pour trouver l'animé 
+  const query = ref ('')
+  // Les animé qu'on ajout a notre list
+  const my_anime = ref ([])
+  // Tout les resulta après le result de notre query
+  const search_results = ref ([])
+
+
+  // Permet de lire de A-Z
+  const my_anime_asc = computed(() => {
+    return my_anime.value.sort((a, b) => {
+      return a.title.localeCompare(b.title)
+    })
   })
-})
 
-// Aller cherche l'api
-const searchAnime = () => {
-  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
-  fetch(url)
-  .then (res => res.json ())
-  .then (res => {
-    search_results.value = res.data
-  })
-}
-
-
-const handleInput = event => {
-  if (!event.target.value) {
-    search_results.value = []
+  // Aller cherche l'api
+  const searchAnime = () => {
+    const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
+    fetch(url)
+    .then (res => res.json ())
+    .then (res => {
+      search_results.value = res.data
+    })
   }
-}
+
+
+  const handleInput = event => {
+    if (!event.target.value) {
+      search_results.value = []
+    }
+  }
 
 
 
-const addAnime = anime => {
-  search_results.value = []
-  query.value = ''
+  const addAnime = anime => {
+    search_results.value = []
+    query.value = ''
 
-  my_anime.value.push({
-    id: anime.mal_id,
-    title: anime.title,
-    image: anime.images.jpg.image_url,
-    total_episodes: anime.episodes,
-    // Vient pas de L'Api
-    watched_episodes: 0
+    my_anime.value.push({
+      id: anime.mal_id,
+      title: anime.title,
+      image: anime.images.jpg.image_url,
+      total_episodes: anime.episodes,
+      // Vient pas de L'Api
+      watched_episodes: 0
+    })
+
+    localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+  }
+
+  const increaseWatch = anime => {
+    anime.watched_episodes++
+    localStorage.setItem('my_anime', JSON.stringify(my.anime.value))
+  }
+
+  const decreaseWatch = anime => {
+    anime.watched_episodes--
+    localStorage.setItem('my_anime', JSON.stringify(my.anime.value))
+  }
+
+  onMounted(() => {
+    my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || []
   })
-
-  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
-}
-
-const increaseWatch = anime => {
-  anime.watched_episodes++
-  localStorage.setItem('my_anime', JSON.stringify(my.anime.value))
-}
-
-const decreaseWatch = anime => {
-  anime.watched_episodes--
-  localStorage.setItem('my_anime', JSON.stringify(my.anime.value))
-}
-
-onMounted(() => {
-  my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || []
-})
 </script>
 
 <template>
+  <Sidebar/>
+  <div :style="{ 'marign-left': sidebarWidth }">
+    <router-view />
+  </div>
   <main>
     <h1>My Anime Tracker</h1>
 
@@ -105,7 +122,7 @@ onMounted(() => {
           {{ anime.watched_episodes }} / {{ anime.total_episodes }}
         </span>
         <button v-if="anime.total_episodes !== anime.watched_episodes" @click="increaseWatch(anime)">+</button>
-        
+
         <button v-if="anime.watched_episodes > 0" @click="decreaseWatch(anime)">-</button>
       </div>
     </div>
---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,70 @@
+<script>
+import { collapsed, toggleSidebar, sidebarWidth} from './state'
+
+
+  export default {
+    props: {},
+    setup() {
+      return { collapsed, toggleSidebar, sidebarWidth }
+    }
+  }
+</script>
+
+<template>
+  <div class="sidebar" :style="{ width: sidebarWidth }">
+    <h1>
+      <span v-if="collapse">
+        <div>V</div>
+        <div>S</div>
+      </span>
+      <span v-else>Vue Sidebar</span>
+    </h1>
+
+    <span class="collapse-icon" @click="toggleSidebar" :class="{ 'rotate-180' : collapse }">
+      <i class='bx bx-right-arrow-alt'></i>
+    </span>
+  </div>
+</template>
+
+<style>
+  :root {
+    --sidebar-bg-color:#cf0a0a;
+    --sidebar-bg-hover:#b80a0a;
+    --sidebar-bg-active:#10e033;
+  }
+</style>
+
+<style scoped>
+  .sidebar {
+    color: white;
+    background-color: var(--sidebar-bg-color);
+
+    float: left;
+    position: fixed;
+    z-index: 1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    padding: 0.5em;
+
+    transition: 0.3s ease;
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .collapse-icon {
+    position: absolute;
+    bottom: 0;
+    padding: 0.75em;
+
+    color: rgba(255, 255, 255, 0.7);
+
+    transition: 0.2s linear;
+  }
+
+  .rotate-180 {
+    transform: rotate(180deg);
+    transition: 0.2s linear;
+  }
+</style>
