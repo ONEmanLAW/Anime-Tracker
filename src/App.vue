0f7b9c94dd2792b7d3c7b5e0d23f1caaaee2d1be
Hugo Culot <hugoculot.dev@gmail.com>
2024-01-31 16:31:50 +0100

    Ajout d'une Search bar, image, titre, descrtipion.

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,101 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// Pour L'API
+import { ref, computed, onMounted } from 'vue'
+
+// Query pour ce que l'on tape pour trouver l'animé 
+const query = ref ('')
+// Les animé qu'on ajout a notre list
+const my_anime = ref ([])
+// Tout les resulta après le result de notre query
+const search_results = ref ([])
+
+
+// Permet de lire de A-Z
+const my_anime_asc = computed(() => {
+  return my_anime.value.sort((a, b) => {
+    return a.title.localeCompare(b.title)
+  })
+})
+
+// Aller cherche l'api
+const searchAnime = () => {
+  const url = `https://api.jikan.moe/v4/anime?q=${query.value}`
+  fetch(url)
+  .then (res => res.json ())
+  .then (res => {
+    search_results.value = res.data
+  })
+}
+
+
+const handleInput = event => {
+  if (!event.target.value) {
+    search_results.value = []
+  }
+}
+
+
+
+const addAnime = anime => {
+  search_results.value = []
+  query.value = ''
+
+  my_anime.value.push({
+    id: anime.mal.id,
+    title: anime.title,
+    image: anime.images.jpg.image_url,
+    total_episodes: anime.episodes,
+    // Vient pas de L'Api
+    watched_episodes: 0
+  })
+
+  localStorage.setItem('my_anime', JSON.stringify(my_anime.value))
+}
+
+const increaseWatch = anime => {
+  anime.watched_episodes++
+  localStorage.setItem('my_anime', JSON.stringify(my.anime.value))
+}
+
+const decreaseWatch = anime => {
+  anime.watched_episodes--
+  localStorage.setItem('my_anime', JSON.stringify(my.anime.value))
+}
+
+onMounted(() => {
+  my_anime.value = JSON.parse(localStorage.getItem('my_anime')) || []
+})
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main>
+    <h1>My Anime Tracker</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <form @submit.prevent="searchAnime">
+      <input 
+        type="text"
+        placeholder="search for a anime ..." 
+        v-model="query" 
+        @input="handleInput" />
+      <button type="submit">Search</button>
+    </form>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div class="results" v-if="search_results.length > 0">
+      <div class="result" v-for="anime in search_results">
+        <img :src="anime.images.jpg.image_url" />
+        <div class="details">
+          <h3> {{ anime.title }}</h3>
+          <p :title="anime.synopsis" v-if="anime.synopsis">
+            {{ anime.synopsis.slice(0, 120) }}
+          </p>
+          <span class="flex-1"></span>
+          <button @click="addAnime(anime)">Add to my Anime</button>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
