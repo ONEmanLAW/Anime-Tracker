f28996ac08d928cf9c2ff4efdacf4262b696d3ad
Hugo Culot <hugoculot.dev@gmail.com>
2024-02-04 17:05:59 +0100

    Added new Stats (Total animé terminé.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -8,6 +8,11 @@
     const animeList = animeStore.getAnimeList();
     return animeList.reduce((total, anime) => total + anime.watched_episodes, 0);
   });
+
+  const totalCompletedAnime = computed(() => {
+    const animeList = animeStore.getAnimeList();
+    return animeList.filter(anime => anime.watched_episodes === anime.total_episodes).length;
+  });
 </script>
 
 
@@ -16,5 +21,6 @@
     <h1>Dashboard</h1>
     <p>Nombre d'animes dans My Anime: {{ animeStore.getAnimeList().length }}</p>
     <p>Nombre total d'épisodes vus: {{ totalWatchedEpisodes }}</p>
+    <p>Nombre total d'animes terminés: {{ totalCompletedAnime }}</p>
   </div>
 </template>
